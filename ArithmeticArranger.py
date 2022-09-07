32c7053821fd6ed688af696640bc5c2b10ddac8b
Rosco18 <112519921+Rosco18@users.noreply.github.com>
2022-09-07 16:34:06 -0600

    Add files via upload

--- ArithmeticArranger.py
+++ ArithmeticArranger.py
@@ -1,6 +1,11 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, result=True):
+
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
     # Create rule for Error if more than 5 problems
-    if len(problems) > 5:
+    if len(problems) > 6:
         return("Error: Too many problems.")
     else:
         for problem in problems:
@@ -9,8 +14,10 @@ def arithmetic_arranger(problems):
             # Check all problems have "+" or "-" only.
             if "+" in problem:
                 problem = problem.split("+")
+                operator = "+"
             elif "-" in problem:
                 problem = problem.split("-")
+                operator = "-"
             else:
                 return("Error: Operator must be a + or a -.")
 
@@ -18,9 +25,26 @@ def arithmetic_arranger(problems):
             if not(problem[0].isnumeric() or problem[1].isnumeric()):
                 return("Error: Numbers only contain digits.")
             # Check problems only contain max 4 digits per number.
-            if len(problem[0]) > 4 or len(problem[1]) > 4:
+            if len(problem[0]) > 6 or len(problem[1]) > 6:
                 return("Error: Numbers can't be more than 4 digits.")
 
+            align = min([len(problem[0]), len(problem[1])]) + 2
 
-# return arranged_problems
-print(arithmetic_arranger(["32 + 698", "32365 + 698", "32 - 698", "32 + 698", "82 + 698"]))
+            if result:
+                answer = str(eval(problem[0] + operator + problem[1]))
+                line4 += answer.center(align) + "    "
+
+            line1 += problem[0].center(align) + "    "
+            line2 += operator + problem[1].center(align - 1) + "    "
+            line3 += "-" * (align) + "    "
+
+        arranged_problems = "\n".join([line1, line2, line3])
+
+        if result:
+            line4 = line4.rstrip()
+            arranged_problems += "\n" + line4 + "\n" + line3 + "    "
+
+        return arranged_problems
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "12356 + 94965"]))
